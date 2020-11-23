8ea13c7fddfcbc08034e809536ce202ff9b905f7
carlos.ferreira@al.infnet.edu.br <carlos.ferreira@al.infnet.edu.br>
2020-11-23 17:36:54 -0300

    Tela inicial

--- app/src/main/java/com/carwilfer/tp3avaliacaoclassificacaoandroid/MainActivity.kt
+++ app/src/main/java/com/carwilfer/tp3avaliacaoclassificacaoandroid/MainActivity.kt
@@ -2,10 +2,46 @@ package com.carwilfer.tp3avaliacaoclassificacaoandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var HintEditTextNumberIdade = editTextNumberIdade.text
+        editTextNumberIdade.hint = "26 anos"
+
+        editTextTextPersonNome.addTextChangedListener(
+            object : TextWatcher{
+                override fun afterTextChanged(s: Editable?) {
+                   //
+                }
+
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+                   // 
+                }
+
+                override fun onTextChanged(text: CharSequence?, start: Int, before: Int, count: Int) {
+               textViewMutavel.text = text
+                }
+
+            }
+        )
+        var HintEditTextTextPersonNome = editTextTextPersonNome.text
+        editTextTextPersonNome.hint = "Carlos Ferreira"
+
+        buttonIniciar.setOnClickListener{
+
+        }
+
+
     }
 }
